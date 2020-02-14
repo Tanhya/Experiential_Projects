30eb5136d1fca0a154269d4d200d54931aea617c
cgordon744 <cgordon744@gmail.com>
2020-02-14 14:18:59 -0700

    Update dataCleaning.R

--- dataCleaning.R
+++ dataCleaning.R
@@ -6,13 +6,16 @@ sil <- sil[sil$ORDER_REASON_ID!='PAR',] #get rid of PAR
 sil <- sil[!is.na(sil$ORIGINAL_ORDER),] #get rid of NULL original orders
 sil <- sil[!is.na(sil$FABRIC_ID),] #get rid of NULL fabric IDs
 sil <- sil[sil$ORDER_REASON_ID=="STD" | sil$ORDER_REASON_ID=="REM" | sil$ORDER_REASON_ID=="REP" |sil$ORDER_REASON_ID=="CON" ,]
+# ^retain 4 needed order reason ID
 sil <- sil[!is.na(sil$OPERATING_SYSTEM_ID),]
-sil <- sil[!is.na(sil$SOLD_TO_ID),]
+sil <- sil[!is.na(sil$SOLD_TO_ID),] # clean out small NA values
 sil <- sil[!(sil$ORIGINAL_ORDER== 230066675421 | sil$SALES_ORDER== 230066675421),]
 sil <- sil[!sil$ORIGINAL_ORDER== 230054119481,]
 sil <- sil[!sil$ORIGINAL_ORDER== 230052926688,]
 sil <- sil[!sil$ORIGINAL_ORDER== 230053010643,]
-sil <- sil[!sil$ORIGINAL_ORDER== 230066612777,]
+sil <- sil[!sil$ORIGINAL_ORDER== 230066612777,] # take out problem orders discovered later in code
+
+# fill NAs as character nulls for STD/CON orders
 sil$ORDER_REASON_ID <- factor(sil$ORDER_REASON_ID)
 sil$RESPONSIBILITY_CODE_ID <- as.character(sil$RESPONSIBILITY_CODE_ID)
 sil$RESPONSIBILITY_CODE_ID[is.na(sil$RESPONSIBILITY_CODE_ID)] <- "NULL"
@@ -20,27 +23,32 @@ sil$REASON_CODE_ID <- as.character(sil$REASON_CODE_ID)
 sil$REASON_CODE_ID[is.na(sil$REASON_CODE_ID)] <- "NULL"
 sil$REASON_CODE <- as.character(sil$REASON_CODE)
 sil$REASON_CODE[is.na(sil$REASON_CODE)] <- "NULL"
-sil$SLAT_SIZE <- NULL
+sil$SLAT_SIZE <- NULL # slat size not relevant to sillouhette
 
+# combine sales order and sales order line
 sil$ORIGINAL_ORDER1 <- paste0(sil$ORIGINAL_ORDER,sil$ORIGINAL_ORDER_LINE)
-#combine sales order and sales order line
 sil$SALES_ORDER1 <- paste0(sil$SALES_ORDER,sil$SALES_ORDER_LINE)
+
 sil <- sil[(sil$ORIGINAL_ORDER %in% sil$SALES_ORDER),] # drop rep/rem/crr orders with no og order
-sil <- sil[!sil$NET_SALES_UNITS<0,]
+sil <- sil[!sil$NET_SALES_UNITS<0,] # remove negative sales units
+
+# remove duplicated combined orders
 dupes <- sil[duplicated(sil$SALES_ORDER1),]
 good_stuff <- sil[!(sil$SALES_ORDER1 %in% dupes$SALES_ORDER1 & sil$NET_SALES_UNITS == 0),]
 sil <- good_stuff[!duplicated(good_stuff$SALES_ORDER1),]
-sil.cp <- sil
-sil1<-merge(sil,sil.cp,by.x='ORIGINAL_ORDER1',by.y='SALES_ORDER1')
-sil[(sil$ORDER_REASON_ID=="REP" | sil$ORDER_REASON_ID=="REM")&!(sil$ORIGINAL_ORDER1 %in% sil$SALES_ORDER1),]
-# ^ checks for orders marked REP or REM that are not in our merged df (i.e. dont have OG order # associated)
-sil$ORIGINAL_ORDER <- as.factor(sil$ORIGINAL_ORDER)
-nrow(sil[sil$ORDER_REASON_ID=="REP" | sil$ORDER_REASON_ID=="REM",])
+sil <- sil[(sil$ORIGINAL_ORDER %in% sil$SALES_ORDER),] # drop rep/rem/crr orders with no og order
 
+#copy and self merge to produce matched STD orders with their REP/REM orders in df sil1
+sil.cp <- sil
+sil1<-merge(sil,sil.cp,by.x='ORIGINAL_ORDER1',by.y='SALES_ORDER1') 
+# sil[(sil$ORDER_REASON_ID=="REP" | sil$ORDER_REASON_ID=="REM")&!(sil$ORIGINAL_ORDER1 %in% sil$SALES_ORDER1),]
+# ^ checks for orders marked REP or REM that are not in our merged df (i.e. dont have OG order # associated)
+
+# calculate lag
+sil$ORIGINAL_ORDER <- as.factor(sil$ORIGINAL_ORDER)
 sil1$lag <- as.Date(as.character(sil1$SO_CREATED_DATE.x), "%Y%m%d") - as.Date(as.character(sil1$SO_CREATED_DATE.y), "%Y%m%d")
 lagdfToInsert <- data.frame(sil1$ORIGINAL_ORDER1,sil1$SALES_ORDER1,sil1$lag)
 lagdfToInsert$sil1.ORIGINAL_ORDER1 <- as.character(lagdfToInsert$sil1.ORIGINAL_ORDER1)
-sil$lag <- NULL
 
 silFinal <- merge(sil, lagdfToInsert, by.x = 'SALES_ORDER1', by.y='sil1.SALES_ORDER1', all.x = TRUE)
 names(silFinal)[names(silFinal) == 'sil1.lag'] <- 'lagDays'
@@ -91,33 +99,9 @@ top50_ID <- silFinal
 top50_ID$ID_50<- ifelse(top50_ID$OPERATING_SYS_OPT_ID %in% top50$x, 1, 0)
 silFinal <- top50_ID[top50_ID$ID_50 == 1,]
 
-#reduce cfirst top 50 reason code id
-silFinal$REASON_CODE_ID <- as.factor(silFinal$REASON_CODE_ID)
-ID_COUNT<- count(silFinal$REASON_CODE_ID)
-ID_sort<-ID_COUNT[order(ID_COUNT$freq, decreasing = T),]
-top50<- head(ID_sort, 50)
-top50_ID <- silFinal
-top50_ID$ID_50<- ifelse(top50_ID$REASON_CODE_ID %in% top50$x, 1, 0)
-silFinal <- top50_ID[top50_ID$ID_50 == 1,]
-
-#reduce cfirst top 50 responsibility code id
-silFinal$RESPONSIBILITY_CODE_ID <- as.factor(silFinal$RESPONSIBILITY_CODE_ID)
-ID_COUNT<- count(silFinal$RESPONSIBILITY_CODE_ID)
-ID_sort<-ID_COUNT[order(ID_COUNT$freq, decreasing = T),]
-top50<- head(ID_sort, 50)
-top50_ID <- silFinal
-top50_ID$ID_50<- ifelse(top50_ID$RESPONSIBILITY_CODE_ID %in% top50$x, 1, 0)
-silFinal <- top50_ID[top50_ID$ID_50 == 1,]
-
-#reduce cfirst top 50 reason code
-silFinal$REASON_CODE <- as.factor(silFinal$REASON_CODE)
-ID_COUNT<- count(silFinal$REASON_CODE)
-ID_sort<-ID_COUNT[order(ID_COUNT$freq, decreasing = T),]
-top50<- head(ID_sort, 50)
-top50_ID <- silFinal
-top50_ID$ID_50<- ifelse(top50_ID$REASON_CODE %in% top50$x, 1, 0)
-silFinal <- top50_ID[top50_ID$ID_50 == 1,]
-
+# drops original standard order rows for orders that failed, because we are marking the 
+# REM/REP orders as failures, and keeping the original line implies that datapoint is actually 
+# a success, which is not accurate
 silFinal <- silFinal[!((silFinal$SALES_ORDER1 %in% silFinal$sil1.ORIGINAL_ORDER1) & (silFinal$ORDER_REASON_ID == "STD" | silFinal$ORDER_REASON_ID == "CON")),]
 
 silFinal$sil1.ORIGINAL_ORDER1 <- NULL
@@ -127,6 +111,7 @@ silFinal$COLOR_ID_50 <- NULL
 silFinal$FABRIC_ID_50 <- NULL
 silFinal$ID_50 <- NULL
 names(silFinal)[names(silFinal) == 'ORIGINAL_ORDER1'] <- 'ORIGINAL_ORDER_COMBINED'
+names(silFinal)[names(silFinal) == 'SALES_ORDER1'] <- 'SALES_ORDER_COMBINED'
 
 
 silFinal$ORIGINAL_MATERIAL_ID <- factor(silFinal$ORIGINAL_MATERIAL_ID)
@@ -139,5 +124,5 @@ silFinal$RESPONSIBILITY_CODE_ID <- factor(silFinal$RESPONSIBILITY_CODE_ID)
 silFinal$REASON_CODE <- factor(silFinal$REASON_CODE)
 
 save(silFinal, file="silFinal.RData")
-colSums(is.na(silFinal))
+
 
